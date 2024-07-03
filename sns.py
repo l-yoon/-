682cc590d7c8270708341a0b2f49db2d9d781d13
윤찬민 <chanminyoon@Mins-MacBook-Pro-2.local>
2024-07-03 15:55:04 +0900

    member instance

---
+++ sns.py
@@ -0,0 +1,14 @@
+#member instance
+member1 = Member("승원","one1122","qw1122")
+member2 = Member("원빈","beanone", "bboo1122")
+member3 = Member("지민","ming22","password98")
+member4 = Member("리나","rina0000","qwerty@")
+
+members = []
+members.append(member1)
+members.append(member2)
+members.append(member3)
+members.append(member4)
+
+for member in members:
+    print(members)
