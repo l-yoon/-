226d17a79c6323711c8e2ec725fb26668b3ca39c
temfarming <temfarming@naver.com>
2024-07-04 13:00:14 +0900

    Update sns.py

--- sns.py
+++ sns.py
@@ -58,7 +58,7 @@ while True:
             break
         member = create_member()
         members.append(member)
-        print(f"{members.name} 회원이 등록되었습니다.")
+        print(f"{member.name} 회원이 등록되었습니다.")
         
 
 print("추가된 회원 목록")
