f90d5eb03df0f787b40b9104c6be18e54d199833
l-yoon <min>
2024-07-05 12:57:09 +0900

    1-6 change

--- sns.py
+++ sns.py
@@ -1,97 +1,115 @@
+import hashlib
 class Member:
-    def __init__(self, name = [], username = [], password = [], members = []):
+    def __init__(self, name, username, password):
         self.name = name
         self.username = username
         self.password = password
-        self.members = members
-    
-    def member_add(self):
-        self.name.append(input('회원 이름을 입력해주세요: '))
-        self.username.append(input('아이디를 입력해주세요: '))
-        self.password.append(input('비밀번호를 입력해주세요: '))
 
     def display(self):
-        # TODO : 코드 구현이 필요합니다.
-        pass
-
-    # members = [[이름1, 아이디1, 비번1], [이름2, 아이디2, 비번2]...]
-    def mems_append(self):
-        for i in range(len(self.name)):
-            self.members.append([self.name[i], self.username[i], self.password[i]])
-
-    # members 리스트를 돌면서 회원들의 이름을 모두 프린트
-    def mems_print(self):
-        for x in range(len(self.members)):
-            print(self.members[x][0])
-
+        print(f"이름: {self.name}, 아이디: {self.username} 비번: {self.password}")
 
 class Post:
-    def __init__(self, title = [], content = [], author = [], posts = []):
-        self.title = title
-        self.content = content
-        self.author = author
-        self.posts = posts
+    def __init__(self, username, title, content):
+        self.author = username  # 인스턴스 변수 author
+        self.title = title  # 인스턴스 변수 title
+        self.content = content  # 인스턴스 변수 content
 
-    def post_add(self):
-        self.title.append(input('피드 제목을 입력해주세요: '))
-        self.content.append(input('피드 내용을 입력해주세요: '))
+    def display(self):
+        print(f"아이디: {self.author}, 타이틀: {self.title}")
 
+def create_member():
+    name = input("이름을 입력하세요: ")
+    username = input("아이디를 입력하세요: ")
+    password = input("비밀번호를 입력하세요: ")
+    # 비밀번호 해시화
+    passwd = hashlib.sha256()
+    passwd.update(password.encode())
+    encoded_pwd = passwd.hexdigest()
+    return Member(name, username, encoded_pwd)
 
+def create_post(author_name):
+    title_name = input("제목을 작성해주세요: ")
+    content_detail = input("내용을 작성해주세요: ")
+    return Post(author_name, title_name, content_detail)
+# member instances
+member1 = Member("승원", "one1122", "qw1122")
+member2 = Member("원빈", "beanone", "bboo1122")
+member3 = Member("지민", "ming22", "password98")
+member4 = Member("리나", "rina0000", "qwerty@")
 
-    # 각각 회원이 게시글 세개 이상 작성하게 하기
-    def three_post(self):
-        unique_author = set(self.author)
-
-        # 중복제거이름 list에서 아이디(중복제거X)의 count 구하기
-
-        while True:     # 이걸 class 안에 넣는다면?
-            for z in unique_author:
-                if self.author.count(z) < 3:
-                    print(f"{z}의 게시글이 부족합니다. 추가해주세요")
-                    # 다시 입력하게 하기
-                    self.author.append(z)
-                    self.title.append(input('피드의 제목을 입력: '))
-                    self.content.append(input('피드의 내용을 입력: '))
-                    break
-            else:
-                break
-
-    # posts = [[제목1, 내용1, 이름1], [제목2, 내용2, 이름2]...]
-    def posts_append(self):
-        for j in range(len(self.title)):
-            self.posts.append([self.title[j], self.content[j], self.author[j]])
-
-
-    # 특정 유저 작성 게시글 '제목'을 모두 프린트
-    def user_title(self):
-        user_post = input('조회할 유저 아이디를 검색해주세요: ')
-
-        for a in self.posts:
-            if str(user_post).strip().lower() in str(a[2]).strip().lower():
-                print(a[0])
-
-    # 특정 단어가 content에 포함된 게시글의 '제목'을 모두 프린트
-    def word_title(self):
-        word_post = input('조회할 특정 단어 게시글 조회: ')
-
-        for b in self.posts:
-            if str(word_post).strip().lower() in str(b[1]).strip().lower():
-                print(b[0])
+# lists
+members = []
+posts = []
+# 5 answer
+members.append(member1)
+members.append(member2)
+members.append(member3)
+members.append(member4)
 
 
-# ----- 코드 실행 ------
-m = Member()
-p = Post()
+# 맴버 추가 반복문
+while True:
+    question = input("새 회원을 등록하시겠습니까? (y/n): ").lower()
+    if question == 'y':
+        new_member = create_member()
+        members.append(new_member)
+        new_member.display()
 
-p.author = m.username
+        # 새 회원에게 세 개의 게시글 작성 요청
+        for i in range(3):
+            print(f"\n새 게시물 {i+1}/3 작성")
+            new_post = create_post(new_member.username)
+            posts.append(new_post)
+            print('글을 작성하셨습니다.')
+            print(f"작성자: {new_post.author}")
+            print(f"제목: {new_post.title}")
+            print(f"내용: {new_post.content}")
+    elif question == 'n':
+        print("다음단계")
+        break
+    else:
+        print("올바른 값을 입력하세요!")
 
-m.member_add()
-m.mems_append()
-m.mems_print()
+while True:
+    question = input("새 게시물을 등록하시겠습니까? (y/n): ").lower()
+    if question == 'y':
+        author_name = input("작성자 이름을 입력하세요: ")
+        new_post = create_post(author_name)
+        posts.append(new_post)
+        # 작성자, 제목, 내용을 다 입력한 경우 출력됨
+        print('글을 작성하셨습니다.')
+        print(f"작성자: {new_post.author}")
+        print(f"제목: {new_post.title}")
+        print(f"내용: {new_post.content}")
 
-p.post_add()
-p.three_post()
-p.posts_append()
+    elif question == 'n':
+        print("다음 단계")
+        break
+    else:
+        print("올바른 값을 입력하세요!")
 
-p.user_title()
-p.word_title()
+# print 회원, 글 목록
+print("회원 목록")
+for member in members:
+    member.display()
+
+print("게시물 목록")
+for post_list in posts:
+    post_list.display()
+
+# 특정 유저 작성 게시글 '제목'을 모두 프린트
+def user_title():
+    user_post = input('조회할 유저 아이디를 검색해주세요: ')
+    for post in posts:
+        if post.author == user_post:
+            print(post.title)
+
+# 특정 단어가 content에 포함된 게시글의 '제목'을 모두 프린트
+def word_title():
+    word_post = input('조회할 특정 단어 게시글 조회: ')
+    for post in posts:
+        if word_post in post.content:
+            print(post.title)
+
+user_title()
+word_title()
